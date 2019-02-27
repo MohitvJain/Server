7a0c0b8930b4f5a0288eb27b27db45b6dbefb666
Mohit Jain <mohit.jain@magicsw.com>
2019-02-27 19:12:32 +0530

    server repo

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    "mongodb://devConnector:VpHmkmSykCpNXdh4@cluster0-shard-00-00-8468d.mongodb.net:27017,cluster0-shard-00-01-8468d.mongodb.net:27017,cluster0-shard-00-02-8468d.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
+  secretOrKey: "secret"
+};
+// VpHmkmSykCpNXdh4
