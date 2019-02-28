c5a6e5c33483856bd2664ef08e7e2325e7ef482b
Mohit Jain <mohit.jain@magicsw.com>
2019-02-28 15:46:42 +0530

    half work

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// Create Schema
 const UserSchema = new Schema({
   name: {
     type: String,
@@ -24,4 +23,4 @@ const UserSchema = new Schema({
   }
 });
 
-module.exports = User = mongoose.model('users', UserSchema);
+module.exports = User = mongoose.model("users", UserSchema);
